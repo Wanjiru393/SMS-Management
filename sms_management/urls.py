12f98df314ce1c17192137db088355a4b088b6ec
Wanjiru393 <carolciru393@gmail.com>
2023-11-21 08:59:34 +0300

    Changing logo appearance

--- sms_management/urls.py
+++ sms_management/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.register_user, name='register_user'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
